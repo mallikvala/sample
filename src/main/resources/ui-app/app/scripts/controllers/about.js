4bb0dc95bb5147f1132276c56264dcc98d83dc86
Mallikarjuna Vala <mallikarjuna.vala@capitalone.com>
2016-03-03 18:21:54 -0500

    UI yeoman seed

---
+++ src/main/resources/ui-app/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name litmusUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the litmusUiApp
+ */
+angular.module('litmusUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ src/main/resources/ui-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name litmusUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the litmusUiApp
+ */
+angular.module('litmusUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
